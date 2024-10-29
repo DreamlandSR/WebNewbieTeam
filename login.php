b369eb69a395be33f0bfccf6edb0638b2286f3b0
Ryan Adi Saputra <158378604+DreamlandSR@users.noreply.github.com>
2024-10-29 21:40:39 +0700

    perubahan pada style siswa

--- login.php
+++ login.php
@@ -1,5 +1,9 @@
 <?php
+require 'vendor/autoload.php';
 session_start();
+
+use NewbieTeam\App\Auth;
+
 $host = 'localhost';
 $db   = 'e_learning';
 $user = 'root'; 
@@ -12,50 +16,32 @@ if ($conn->connect_error) {
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Cek apakah kedua field telah diisi
     if (isset($_POST['nama']) && isset($_POST['password'])) {
-        $nama = $conn->real_escape_string($_POST['nama']);
-        $password = $_POST['password']; 
-        
-        //cek username
-        $sql = "SELECT * FROM users WHERE nama='$nama'";
-        $result = $conn->query($sql);
+        $auth = new Auth($conn);
+        $user = $auth->login($_POST['nama'], $_POST['password']);
 
-        if ($result->num_rows > 0) {
-            // Ambil data user
-            $user = $result->fetch_assoc();
+        if ($user) {
+            // Simpan nama dan sebagai ke session
+            $_SESSION['nama'] = $user['nama'];
+            $_SESSION['sebagai'] = $user['sebagai'];
 
-            // Verifikasi password 
-            if (password_verify($password, $user['password'])) {
-                // Simpan nama dan sebagai ke session
-                $_SESSION['nama'] = $user['nama'];
-                $_SESSION['sebagai'] = $user['sebagai'];
-                
-                // Redirect berdasarkan "sebagai"
-                if ($user['sebagai'] === 'admin') {
-                    header("Location: admin.html");
-                } else if ($user['sebagai'] === 'guru') {
-                    header("Location: guru.html");
-                } else if ($user['sebagai'] === 'siswa') {
-                    header("Location: siswa.html");
-                } else {
-                    header("Location: login.html?error=unknown_sebagai");
-                }
-                exit();
+            // Redirect
+            if ($user['sebagai'] === 'admin') {
+                header("Location: admin.html");
+            } else if ($user['sebagai'] === 'guru') {
+                header("Location: guru.html");
+            } else if ($user['sebagai'] === 'siswa') {
+                header("Location: siswa.html");
             } else {
-                // Password salah
-                header("Location: login.html?error=wrong_credentials");
-                exit();
+                header("Location: login.html?error=unknown_sebagai");
             }
+            exit();
         } else {
-            // Nama pengguna tidak ditemukan
             header("Location: login.html?error=wrong_credentials");
             exit();
         }
     } else {
-        // Jika field tidak diisi
         header("Location: login.html?error=empty_fields");
         exit();
     }
 }
-?>
